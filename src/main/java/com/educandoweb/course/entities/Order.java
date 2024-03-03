47a9e8dfa2e99a1f8f6474a63ea0cc0e13025e28
henriquepablo <pablo269704@gmail.com>
2024-03-03 14:45:51 -0300

    feat: implementation order, instant iso 8601 - part 2

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,18 +17,24 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_order")
-public class Order implements Serializable{
-	
+public class Order implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private Instant moment;
 	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	private Instant moment;
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+
+	public Order() {
+
+	}
 
 	public Order(Long id, Instant moment, User client) {
 		this.id = id;
@@ -73,8 +82,5 @@ public class Order implements Serializable{
 		Order other = (Order) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
+
 }
---
+++ src/main/java/com/educandoweb/course/resources/OrderResource.java
@@ -0,0 +1,36 @@
+package com.educandoweb.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.services.OrderService;
+
+@RestController // fala que a classe vai responder requisições
+@RequestMapping(value = "/orders") // fala a rota que vai ser respondida
+public class OrderResource {
+	
+	@Autowired
+	private OrderService service;
+	
+	// ResponseEntity -> representa uma resposta de uma requisição
+	// GetMapping -> diz que vai responder uma requisição do tipo get
+	@GetMapping
+	public ResponseEntity<List<Order>> findAll() {
+		List<Order> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Order> findById(@PathVariable Long id) {
+		Order obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
